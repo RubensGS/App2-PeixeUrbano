08666ad6d90b8196b1e8adc244b31ba7eee2e051
RubensGS <rubensgoncalvessouza@gmail.com>
2018-03-02 14:30:22 -0300

    Criando Observable e utilizando subscrible e unsubscrible

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { OfertasService } from '../ofertas.service';
 import { Oferta } from '../shared/oferta.model';
+import { Observable } from 'rxjs/Observable';
+// tslint:disable-next-line:import-blacklist
+import 'rxjs/Rx';
+import { Observer } from 'rxjs/Observer';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-oferta',
@@ -9,7 +14,10 @@ import { Oferta } from '../shared/oferta.model';
   styleUrls: ['./oferta.component.css'],
   providers: [ OfertasService ]
 })
-export class OfertaComponent implements OnInit {
+export class OfertaComponent implements OnInit, OnDestroy {
+
+  public tempoObservableSubscription: Subscription;
+  public meuObservableTesteSubscription: Subscription;
 
   public oferta: Oferta;
 
@@ -21,6 +29,10 @@ export class OfertaComponent implements OnInit {
     this.ofertasService.getOfertasPorId(this.route.snapshot.params['id'])
       .then((oferta: Oferta) => this.oferta = oferta);
 
+    // this.route.params.subscribe((param: any) => console.log(param),
+    //                             (erro: any) => console.log(erro),
+    //                             () => console.log('Processamento classificado como concluído'));
+
     /*
         fica "escutando" alterações na rota. Quando uma alteração é "escutada"
         podemos reagir a mudança (reactive programming)
@@ -28,6 +40,35 @@ export class OfertaComponent implements OnInit {
     // this.route.params.subscribe((parametro: any) => {
     //   console.log(parametro.id);
     // });
+
+    // tslint:disable-next-line:prefer-const
+    let tempo = Observable.interval(2000);
+
+    this.tempoObservableSubscription = tempo.subscribe((intervalo: number) => console.log(intervalo));
+
+    // observable (observável)
+    // tslint:disable-next-line:prefer-const
+    let meuObservableteste = Observable.create((observer: Observer<number>) => {
+      observer.next(1);
+      observer.next(2);
+      observer.complete();
+      observer.error('Algum erro foi encontrado na stream de eventos');
+    });
+
+    // observable (observador)
+    this.meuObservableTesteSubscription = meuObservableteste.subscribe(
+      // tratar fluxo de dados
+      (resp) => console.log(resp),
+      // tratar erros
+      (erro) => console.log(erro),
+      // tratar finalização da stream
+      () => console.log('Stream foi finalizada')
+      );
+  }
+
+  ngOnDestroy() {
+    this.meuObservableTesteSubscription.unsubscribe();
+    this.tempoObservableSubscription.unsubscribe();
   }
 
 }
